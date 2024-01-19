5f795ad3cfc93e8701de5cf16e7c7bcfdbb08e02
Sebastian Scherbes <github@scherbes.de>
2024-01-19 11:01:20 +0800

    add code to LoginSchema

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,6 +3,7 @@ import * as z from "zod";
 export const LoginSchema = z.object({
 	email: z.string().email(),
 	password: z.string().min(1, { message: "Password is required" }),
+	code: z.optional(z.string()),
 });
 
 export const RegisterSchema = z.object({
