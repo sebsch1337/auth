7cd6392217230cd445888bcdfd409eea38e0e27b
Sebastian Scherbes <github@scherbes.de>
2024-01-22 12:54:57 +0800

    create serverside session functions

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,15 @@
+// To get the session SERVER-SIDE (server components, server actions, api routes)
+
 import { auth } from "@/auth";
 
 export const currentUser = async () => {
 	const session = await auth();
 
 	return session?.user;
+};
+
+export const currentRole = async () => {
+	const session = await auth();
+
+	return session?.user?.role;
 };
