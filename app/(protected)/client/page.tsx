0e6806e46a22c75fba67f1c994b2d5bd38c34bdc
Sebastian Scherbes <github@scherbes.de>
2024-01-22 12:13:46 +0800

    create client page

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { UserInfo } from "@/components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+const ClientPage = () => {
+	const user = useCurrentUser();
+	return (
+		<UserInfo
+			user={user}
+			label="🖥️ Client component"
+		/>
+	);
+};
+
+export default ClientPage;
