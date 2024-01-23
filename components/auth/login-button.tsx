c45f02c416362175954ed16e73419b3fc257efb8
Sebastian Scherbes <github@scherbes.de>
2024-01-23 11:38:31 +0800

    add modal dialog to LoginButton component

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -3,6 +3,9 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
+import { LoginForm } from "@/components/auth/login-form";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
 interface LoginButtonProps {
 	children: React.ReactNode;
 	mode?: "modal" | "redirect";
@@ -12,12 +15,15 @@ interface LoginButtonProps {
 export const LoginButton = ({ children, mode = "redirect", asChild }: LoginButtonProps) => {
 	const router = useRouter();
 
-	// const onClick = () => {
-	// 	router.push("/auth/login");
-	// };
-
 	if (mode === "modal") {
-		return <span>TODO: Implement modal</span>;
+		return (
+			<Dialog>
+				<DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+				<DialogContent className="p-0 w-auto bg-transparent border-none">
+					<LoginForm />
+				</DialogContent>
+			</Dialog>
+		);
 	}
 
 	return (
