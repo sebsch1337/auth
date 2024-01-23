bd98cdaf151402c35e1b5b08ca3296f2638d9591
Sebastian Scherbes <github@scherbes.de>
2024-01-23 11:33:45 +0800

    add isOAuth to user type

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,9 +1,11 @@
 import { UserRole } from "@prisma/client";
+
 import NextAuth, { type DefaultSession } from "next-auth";
 
 export type ExtendedUser = DefaultSession["user"] & {
 	role: UserRole;
 	isTwoFactorEnabled: boolean;
+	isOAuth: boolean;
 };
 
 declare module "next-auth" {
