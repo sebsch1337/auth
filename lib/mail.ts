ec58a45ffaaaae40d8a33f7c17f2c473c1176c31
Sebastian Scherbes <github@scherbes.de>
2024-01-24 11:52:46 +0800

    add sender constant

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,14 +1,14 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
 const domain = process.env.NEXT_PUBLIC_APP_URL;
+const sender = "hello@philippinesuncovered.com";
 
 export const sendVerificationEmail = async (email: string, token: string) => {
 	const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
 	await resend.emails.send({
-		from: "hello@philippinesuncovered.com",
+		from: sender,
 		to: email,
 		subject: "Confirm your Email address",
 		html: `<p>Click <a href=${confirmLink}>here</a> to confirm your Email address.</p>`,
@@ -19,7 +19,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 	const resetLink = `${domain}/auth/new-password?token=${token}`;
 
 	await resend.emails.send({
-		from: "hello@philippinesuncovered.com",
+		from: sender,
 		to: email,
 		subject: "Reset your password",
 		html: `<p>Click <a href=${resetLink}>here</a> to reset your password.</p>`,
@@ -28,7 +28,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 	await resend.emails.send({
-		from: "onboarding@resend.dev",
+		from: sender,
 		to: email,
 		subject: "2FA Code",
 		html: `<p>Your 2FA Code: ${token}</p>`,
